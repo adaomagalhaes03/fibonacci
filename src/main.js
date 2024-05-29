1672897e4663efacb564420fb22b0e522d6587e9
adaomagalhaes03 <adaomagalhaes793@gmail.com>
2024-05-29 22:16:42 +0100

    up

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 var antecessor = 0;
 var sucessor = 1;
 
-for(var i = 0; i<10; i++){
+for(var i = 0; i<100; i++){
     console.log(antecessor)
     console.log(sucessor)
-
+     //formula de fibonacci fn
     antecessor = antecessor + sucessor;
     sucessor= antecessor + sucessor;
 }
